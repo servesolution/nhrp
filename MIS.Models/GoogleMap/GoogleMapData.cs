72ebf58b11ade9f88ee3513ad885bf2acc63e571
servesolution <info@ssith.com.np>
2018-04-17 17:43:33 +0545

    new

---
+++ MIS.Models/GoogleMap/GoogleMapData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MIS.Models.GoogleMap
+{
+   public class GoogleMapData
+    {
+
+       public string DistrictId { get; set; }
+       public string VCDId { get; set; }
+       public string Ward { get; set; }
+       public string Type { get; set; }
+       public string Latitude { get; set; }
+       public string Longitude { get; set; }
+       public string houseID { get; set; }
+       public string houseOwnerName { get; set; }
+
+       public string Targeted { get; set; }
+       public string enrolled { get; set; }
+       public string Payment { get; set; }
+       public string Zoom { get; set; }
+       public List<GoogleMapModel> lstModel { get; set; }
+
+    }
+}
---
+++ MIS.Models/GoogleMap/GoogleMapModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MIS.Models.GoogleMap
+{
+   public class GoogleMapModel
+    {
+       public string houseID { get; set; }
+       public string houseOwnerName { get; set; }
+       public string BeneficiaryName { get; set; }
+       public string Targeted { get; set; }
+       public string enrolled { get; set; }
+       public string Payment { get; set; }
+       public string Latitude { get; set; }
+       public string Longitude { get; set; }
+       public string Type { get; set; }
+
+
+
+    }
+}
